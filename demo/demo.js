da4e7d6e630a82f6189c14379e464bb33a7a52b7
mephraim <me@mattephraim.com>
2010-07-21 01:07:31 -0500

    Add a demo to the project
    Add in a simple demo that shows how the library can be used.

---
+++ demo/demo.js
@@ -0,0 +1,122 @@
+window.onload = function() {
+  // Create a new Animator that is attached to the page's canvas element
+  var animator = CandidCanvas.createAnimator(document.getElementById('canvas'));
+
+  animator.addScenes(Scene1.get(),
+                     Scene2.get());
+  animator.loop();
+
+  addEventHandlers(animator);
+};
+
+function addEventHandlers(anim) {
+  document.getElementById("play").addEventListener('click', function() {
+    anim.play();
+  });
+
+  document.getElementById("pause").addEventListener('click', function() {
+    anim.pause();
+  });
+
+  document.getElementById("loop").addEventListener('click', function() {
+    anim.loop();
+  });
+
+  document.getElementById("stop").addEventListener('click', function() {
+    anim.reset();
+  });
+}
+
+var Scene1 = {
+  get: function() {
+    // Create a new scene that will last 10 seconds
+    var scene = CandidCanvas.createScene({ duration: 3000 });
+
+    // Add an element that draws the background for each tick
+    scene.addElement(General.getDrawBackground("#080808"));
+
+    // Add an element that draws a rotating circle
+    scene.addElement(Scene1.getCircleDraw(180, 540, 50, "#D2EC4C"));
+
+    // Add an element that draws another rotating circle
+    scene.addElement(Scene1.getCircleDraw(0, 360, 40, "#4CCDED"));
+
+    // Finally, one more circle, but rotate it faster
+    scene.addElement(Scene1.getCircleDraw(90, 810, 30, "#F39B3E"));
+
+    return scene;
+  },
+
+
+  // This function builds a function that will be used as a scene element
+  // that draws circle. The scene element will behave differently, depending
+  // on the params that are passed to function that builds the element.
+  //
+  // See the usage above for different parameters being passed in to build
+  // slightly different scene elements.
+  getCircleDraw: function (startAngle, endAngle, circleRadius, color) {
+    var totalAngleChange = endAngle - startAngle;
+    var rotationRadius = 150;
+
+    return function(anim) {
+      var ctx = anim.context();
+      var scene = anim.currentScene();
+
+      var rotationAmt = (totalAngleChange / scene.duration()) * scene.timeElapsed();
+      var currentAngle = startAngle + rotationAmt;
+
+      var point = Scene1.getCoordsFromAngle(rotationRadius, currentAngle);
+
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.arc(200 + point.x, 200 + point.y, circleRadius, 0, Math.PI * 2, true);
+      ctx.fill();
+    };
+  },
+
+  getCoordsFromAngle: function (radius, angle) {
+    var angleInRads = angle * (Math.PI / 180);
+
+    return { x: radius * Math.cos(angleInRads), 
+             y: radius * Math.sin(angleInRads) };
+  }
+};
+
+var Scene2 = {
+  get: function() {
+    var scene = CandidCanvas.createScene({duration: 1000});
+    scene.addElements(General.getDrawBackground("#677C67"),
+                      Scene2.drawText);
+  
+    return scene;
+  },
+
+  // A function to draw some text to the screen
+  drawText: function(anim) {
+    var ctx = anim.context();
+    ctx.fillStyle    = "#FFF";
+    ctx.font         = "50px Helvetica";
+    ctx.textAlign    = "center";
+    ctx.textBaseline = "middle";
+
+    ctx.fillText("SCENE 2", 200, 200);
+  }
+};
+
+var General = {
+  // This function builds a function that will be used to draw the background
+  // for an animation.
+  getDrawBackground: function(color) {
+    return function(anim) {
+      var ctx = anim.context();
+      ctx.restore();
+      ctx.save();
+
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, 400, 400);
+
+      ctx.restore();
+      ctx.save();
+    };
+  }
+};
